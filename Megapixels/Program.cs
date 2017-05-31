742f2eafd25efc4afa3bcee06b7825eae163b7c4
TeodorDonchev <teodorgdo@gmail.com>
2017-05-31 23:20:09 +0300

    Added new feature

--- Megapixels/Program.cs
+++ Megapixels/Program.cs
@@ -10,13 +10,21 @@ namespace Megapixels
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Please enter the year you made the photo: ");
+            var year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the month you made the photo: ");
+            var month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the day you made the photo: ");
+            Console.Write("Please enter the width of the photo: ");
             var width = double.Parse(Console.ReadLine());
+            Console.Write("Please enter the height of the photo: ");
             var height = double.Parse(Console.ReadLine());
 
             double megapixels = width * height / 1000000;
-
+            Console.WriteLine($"Photo made on: {day}/{month}/{year}");
             Console.WriteLine($"{width}x{height} => {Math.Round(megapixels, 1 )}MP");
+
+
         }
     }
 }
